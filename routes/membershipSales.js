35596698fce35623cc06ca157a67e2ad9c64dbad
CarlosMejiaDev <149196080+CarlosMejiaDev@users.noreply.github.com>
2024-02-21 12:35:36 -0600

    Add files via upload

--- routes/membershipSales.js
+++ routes/membershipSales.js
@@ -3,36 +3,28 @@
 const express = require('express');
 const router = express.Router();
 const MembershipSale = require('../models/MembershipSale');
-const jwt = require('jsonwebtoken');
 
 router.post('/assign', async (req, res) => {
-    try {
-      const token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, 'tu_secreto_jwt');
-      const adminID = decoded.id;
-  
-      const { member_id, membership_id, price } = req.body;
-      const sale = {
-        membership_id,
-        admin_id: adminID,
-        member_id,
-        price
-      };
-  
-      const newSale = await MembershipSale.create(sale);
-      res.status(201).json(newSale);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  });
-  
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const { member_id, membership_id, price } = req.body;
+    const sale = {
+      membership_id,
+      member_id,
+      price
+    };
+
+    const newSale = await MembershipSale.create(sale, token);
+    res.status(201).json(newSale);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 router.get('/', async (req, res) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, 'tu_secreto_jwt');
-    const adminID = decoded.id;
-
-    const sales = await MembershipSale.getAll(adminID);
+    const sales = await MembershipSale.getAll(token);
     res.json(sales);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -42,10 +34,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, 'tu_secreto_jwt');
-    const adminID = decoded.id;
-
-    const newSale = await MembershipSale.create({ ...req.body, admin_id: adminID });
+    const newSale = await MembershipSale.create(req.body, token);
     res.status(201).json(newSale);
   } catch (err) {
     res.status(400).json({ message: err.message });
